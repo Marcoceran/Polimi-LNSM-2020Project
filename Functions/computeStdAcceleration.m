d9d394b8adef22665fb7cd3cec11f7f89b7e7f89
Marco Ceran <62356592+Marcoceran@users.noreply.github.com>
2020-09-08 18:22:46 +0200

    Update computeStdAcceleration.m

--- Functions/computeStdAcceleration.m
+++ Functions/computeStdAcceleration.m
@@ -13,8 +13,8 @@ for a = 1:size(Acceleration, 2)
     for i = 1:size(Acceleration{1, 1}, 1)
         for j = 1:size(Acceleration{1, 1}, 2)
         
-            if(abs(Acceleration{a}(i,j)) >1)
-                wrongTraj(a) = a;
+            if(abs(Acceleration{a}(i,j)) >1)       % we exclude the trajectories that have high accelerations due to changing sign of the velocity after meeting the border of
+                wrongTraj(a) = a;                  % the square in which the trajectories lie
             end
         
         end
@@ -30,10 +30,10 @@ end
 
 for j = 1:size(Acceleration, 2)
     
-        stdAcc(:,j)  = std(Acceleration{j}, 0, 1);   
+        stdAcc(:,j)  = std(Acceleration{j}, 0, 1);  % standard deviation of the accelerations
         
 end
-stdAcc ( :, ~any(stdAcc, 1) ) = [];
+stdAcc ( :, ~any(stdAcc, 1) ) = [];                 % we exclude the ones with std deviation equal to zero
 
 meanSigma = round(mean(stdAcc, 2),2);
 meanStdAcc = meanSigma(1);

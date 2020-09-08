16dd382b49b6dab99a4ec1663929faf5f7c0e500
Marco Ceran <62356592+Marcoceran@users.noreply.github.com>
2020-09-08 12:42:54 +0200

    Update computeCovMat.m

--- Functions/computeCovMat.m
+++ Functions/computeCovMat.m
@@ -1,7 +1,7 @@
 function CovMat = computeCovMat(NumOfAP, rhoUEAP, UE, PosOfAP)
 
 errTOA = zeros(size(rhoUEAP, 1), NumOfAP);
-StdMatrix = zeros(NumOfAP, NumOfAP);
+StdMatrix = zeros(NumOfAP, NumOfAP);    % we define a square matrix with dimensions equal to the number of APs
 
 %AP are in the same position of the task-1a
 distanceUEAP = sqrt ( sum((PosOfAP - UE).^2, 2));
@@ -9,10 +9,10 @@ distanceUEAP = sqrt ( sum((PosOfAP - UE).^2, 2));
 for b = 1:NumOfAP
     
     for a = 1:size(rhoUEAP,1)
-        errTOA(a,b) = abs(distanceUEAP(b) - rhoUEAP(a,b)); 
+        errTOA(a,b) = abs(distanceUEAP(b) - rhoUEAP(a,b));  % we compute a vector of 
     end
     
-    StdMatrix(b,b) = round(mean(errTOA(:,b)));
+    StdMatrix(b,b) = round(mean(errTOA(:,b)));      % we round the results to integer values
     
 end
 

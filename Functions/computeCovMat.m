15ba10ff94d0a1f9cdce99e674c8b9df571d08ab
Marco Ceran <62356592+Marcoceran@users.noreply.github.com>
2020-09-08 12:43:55 +0200

    Update computeCovMat.m

--- Functions/computeCovMat.m
+++ Functions/computeCovMat.m
@@ -9,7 +9,7 @@ distanceUEAP = sqrt ( sum((PosOfAP - UE).^2, 2));
 for b = 1:NumOfAP
     
     for a = 1:size(rhoUEAP,1)
-        errTOA(a,b) = abs(distanceUEAP(b) - rhoUEAP(a,b));  % we compute a vector of 
+        errTOA(a,b) = abs(distanceUEAP(b) - rhoUEAP(a,b));  % we compute a vector of differences between the measurements in the rhoUEAP vector and the ones taken analytically
     end
     
     StdMatrix(b,b) = round(mean(errTOA(:,b)));      % we round the results to integer values

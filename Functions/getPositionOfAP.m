e767d70aff9527c5d5bf59800925937c9171bcd4
Marco Ceran <62356592+Marcoceran@users.noreply.github.com>
2020-09-08 16:02:03 +0200

    Commented some lines

--- Functions/getPositionOfAP.m
+++ Functions/getPositionOfAP.m
@@ -1,5 +1,6 @@
 function posOfAP = getPositionOfAP(NumOfAP, rhoUEAP, UE)
-
+% NumOfAP is the number of access points, rhoUEAP is the vector that contains the TOA and AOA
+% measurements of the user equipment whose coordinates are stored in the vector UE, which is [0 0] in this case
 rhoTOA = rhoUEAP(:,1);
 rhoAOA = rhoUEAP(:,2);
 
@@ -7,9 +8,12 @@ posOfAP = zeros(size(rhoUEAP, 1),2);
 
 for a = 1:NumOfAP
     z = rhoTOA(a) * exp(1i * rhoAOA(a));
-   
+    % we combine the angle and the magnitude measurements in the vector to form complex numbers
+    % that we subsequently subtract from the UE position
     posOfAP(a,1) = round(UE(1) - real(z));
     posOfAP(a,2) = round(UE(2) - imag(z));
+    % and we get the x and y coordinates as the real and imaginary part of those complex numbers
+    % the positions of the APs have to be integer numbers
 end
 
 end
